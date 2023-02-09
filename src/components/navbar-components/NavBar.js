ee8969f2693208318a13f14f5d5e2fb2257c4e75
francis <jerylfranz460@gmail.com>
2023-02-09 11:13:32 +0400

    STYLE: add color to nav

--- src/components/navbar-components/NavBar.js
+++ src/components/navbar-components/NavBar.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export const NavBar = () =>{
    return(
-    <nav className="h-24 bg-black flex">
-        <h1 className="text-white">Weather App</h1>
+    <nav className="h-24 bg-[#2e2e2e] uppercase flex justify-between items-center px-10">
+        <h1 className="text-white ">Weather App</h1>
         <ul className="text-white flex gap-x-6">
             <li><button>Home</button></li>
             <li><button>Map</button></li>
