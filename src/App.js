d3bf343d84a376cd76d699beb781375282555db6
francis <jerylfranz460@gmail.com>
2023-02-09 21:42:30 +0400

    REF: add react routers and route to forecast and map

--- src/App.js
+++ src/App.js
@@ -3,18 +3,25 @@ import { NavBar } from './components/navbar-components/NavBar';
 import { MainContents } from './components/main-contents-component/MainContents';
 import { Current } from './components/current-components/Current';
 import { Forecast } from './components/daily-hourly-components/Forecast';
+import { BrowserRouter, Route ,Routes} from 'react-router-dom';
+import { Map } from './components/map-components/Map';
 
 function App() {
   return (
     
+    <BrowserRouter>
     <div className="App h-screen flex flex-col">
       <NavBar/>
       <MainContents>
         <Current/>
         {/* route forecast and map component */}
-        <Forecast/>
+        <Routes>
+          <Route path='/' element={<Forecast/>}/>
+          <Route path='/map' element={<Map/>}/>
+        </Routes>
         </MainContents>
     </div>
+    </BrowserRouter>
   );
 }
 
