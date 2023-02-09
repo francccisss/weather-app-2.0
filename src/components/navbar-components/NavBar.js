b60631144023714a2b6b1212a1969ec69c261ef0
francis <jerylfranz460@gmail.com>
2023-02-09 13:13:53 +0400

    STYLE: add responsive text and fixed width for search bar

--- src/components/navbar-components/NavBar.js
+++ src/components/navbar-components/NavBar.js
@@ -4,13 +4,13 @@ import "./NavBar.css"
 export const NavBar = () =>{
    return(
     <nav className="h-24 bg-[#2e2e2e] uppercase flex justify-between items-center px-10">
-        <h1 className="text-5xl font-bold text-white ">Weather App</h1>
-        <form onSubmit={(e)=>{
+        <h1 id="logo" className="font-bold whitespace-nowrap text-white ">Weather App</h1>
+        <form className="w-[500px] mr-20 ml-20" onSubmit={(e)=>{
             e.preventDefault()
         }}>
-           <input type="search"/> 
+           <input className="w-[100%] h-10" type="search"/> 
         </form>
-        <ul id="nav-link-list" className="text-2xl font-semibold text-white flex gap-x-6">
+        <ul id="nav-link-list" className="text-[] font-semibold text-white flex gap-x-6">
             <li className="links"><button>Home</button></li>
             <li className="links"><button>Map</button></li>
             <li className="links"><button>GITHUB</button></li>
