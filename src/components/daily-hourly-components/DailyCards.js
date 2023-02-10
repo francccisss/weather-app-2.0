3909bc2807d58c6e3e0ade812bb11700374003f4
francis <jerylfranz460@gmail.com>
2023-02-11 02:26:21 +0400

    STYLE: add drop shadow

--- src/components/daily-hourly-components/DailyCards.js
+++ src/components/daily-hourly-components/DailyCards.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "./dailyCards.css"
          
 export const DailyCards = ({data})=>{
-    return<div className="daily-cards flex-1 flex border-2 border-black justify-center items-center"> 
+    return<div className="daily-cards flex-1 flex bg-[#c4d1eb] border-2 border-black justify-center items-center"> 
     Card {data}
     </div>
 }        
