48fabee4ddd891dcbeea6e7421a72ce3193d2276
francis <jerylfranz460@gmail.com>
2023-02-10 17:22:35 +0400

    REF: add placeholders

--- src/components/current-components/Current.js
+++ src/components/current-components/Current.js
@@ -5,6 +5,12 @@ export const Current =({children}) =>{
     return(
         <div className="flex-1" id="current-container">
             {children}
+            <div id="current-description">
+                <p>Today's weather conditions and forecast for Charlotte, NC.</p>
+                <p>Percipitation: 0%</p>
+                <p>Wind: 15mph</p>
+                <p>Humidity: 54%</p>
+            </div>
         </div>
     )
 }
