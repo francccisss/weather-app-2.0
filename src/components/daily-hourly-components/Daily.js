88b41908d422bbc44b9f23482a2e8236b9d2f5d2
francis <jerylfranz460@gmail.com>
2023-02-11 01:42:50 +0400

    STYLE: centered data on cards and adjusted padding for container

--- src/components/daily-hourly-components/Daily.js
+++ src/components/daily-hourly-components/Daily.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Daily = ({children})=>{
     return(
-    <div id="daily-container" className="flex flex-grow">
+    <div id="daily-container" className="flex gap-x-2 flex-1">
         {children} 
     </div>)
 }
--- src/components/daily-hourly-components/DailyCards.js
+++ src/components/daily-hourly-components/DailyCards.js
@@ -1,7 +1,7 @@
 import React from "react";
          
 export const DailyCards = ({data})=>{
-    return<div className="daily-cards flex-1"> 
+    return<div className="daily-cards flex-1 flex border-2 border-black justify-center items-center"> 
     Card {data}
     </div>
 }        
