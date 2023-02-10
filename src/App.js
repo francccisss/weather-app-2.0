bbef4f4de1fb8dda8a07fce1dea8878d9cd4e5a8
francis <jerylfranz460@gmail.com>
2023-02-10 17:07:14 +0400

    REF: contained route map

--- src/App.js
+++ src/App.js
@@ -2,23 +2,28 @@ import './App.css';
 import { NavBar } from './components/navbar-components/NavBar';
 import { MainContents } from './components/main-contents-component/MainContents';
 import { Current } from './components/current-components/Current';
-import { Forecast } from './components/daily-hourly-components/Forecast';
 import { BrowserRouter, Route ,Routes} from 'react-router-dom';
-import { Map } from './components/map-components/Map';
 import { ROUTES } from './components/routes';
+import { useEffect } from 'react';
 
 function App() {
+
+
+  useEffect(()=>{
+  },[])
+
+  const DISPLAY_ROUTE = ROUTES.map(route=>{
+          return <Route key={route.pageName} path={route.path} element={route.element} /> 
+  })
+
   return (
-    
     <BrowserRouter>
     <div className="App h-screen flex flex-col">
       <NavBar/>
       <MainContents>
         <Current/>
         <Routes>
-          {ROUTES.map(route=>{
-            return <Route key={route.pageName} path={route.path} element={route.element} />
-          })}
+          {DISPLAY_ROUTE}
         </Routes>
         </MainContents>
     </div>
