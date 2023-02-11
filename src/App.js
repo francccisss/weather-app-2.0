df99c816c96ab914293861f44a0d657dd03138e6
francis <jerylfranz460@gmail.com>
2023-02-12 00:39:53 +0400

    REF: removed react query.. dunno how to use it and opted to use fetch api instead

--- src/App.js
+++ src/App.js
@@ -5,19 +5,41 @@ import { Current } from './components/current-components/Current';
 import { BrowserRouter, Route ,Routes} from 'react-router-dom';
 import { ROUTES } from './components/routes';
 import { CurrentHeading } from './components/current-components/CurrentHeading';
-import { useQueryClient } from 'react-query';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   
-  const [searchQuery, setSearchQuery] = useState();
-
+  const [searchQuery, setSearchQuery] = useState("Dubai");
+  const [currentLocation, setCurrentLocation] = useState({});
   const placeHolders = {
     location: "Dubai, Uae",
-  currentTemp: 30,
-  minMax: [27,32]
+    currentTemp: 30,
+    minMax: [27,32]
   }
 
+
+  async function getLocationKey(){
+    try {
+    const fetchLocationKey = await fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=%09IlRAHY0huRuA8lDzfLPGFOWT9u6rybSX&q=${searchQuery}`)
+    const locationKey = await fetchLocationKey.json()
+    // since accuweather's search api returns an array of city by rank we'll be retreiving only the first element
+    return locationKey[0]
+
+    } catch (error) {
+       throw error
+    }
+  }
+
+  useEffect(()=>{
+    getLocationKey().then((key)=>{
+      setCurrentLocation(key)
+    })
+   
+  },[searchQuery])
+
+  useEffect(()=>{
+    console.log(currentLocation)
+  },[currentLocation])
 
   const DISPLAY_ROUTE = ROUTES.map(route=>{
           return <Route key={route.pageName} path={route.path} element={route.element} /> 
--- src/index.js
+++ src/index.js
@@ -2,16 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { ReactQueryDevtools } from 'react-query/devtools'
-import { QueryClient, QueryClientProvider } from 'react-query';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
- const queryClient = new QueryClient()
 
 root.render(
-    <QueryClientProvider client={queryClient}>
     <App />
-    <ReactQueryDevtools initialIsOpen={false}/>
-    </QueryClientProvider>
 );
 
